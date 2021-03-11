9110ac1a6ebbcb4099519eb2b8ad15c61217c15f
Karan Singh <karan100194@gmail.com>
2021-03-11 08:09:42 -0500

    Initial commit

---
+++ src/main/java/com/example/North/SettingsAPI/SettingsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.North.SettingsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettingsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettingsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/North/SettingsAPI/SettingsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.North.SettingsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
